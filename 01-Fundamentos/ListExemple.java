86c62a5d4f3cc3c994e212dd8d5f54e26590c637
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-04-18 10:45:42 -0300

    ajust

---
+++ 01-Fundamentos/ListExemple.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListExemple {
+
+  public static void main(String[] args) {
+
+    List<String> names = new ArrayList<>();
+    names.add("John");
+    names.add("Mary");
+    names.add("Alice");
+
+    System.out.println(names.get(1)); // Utilizando o método get para pegar o valor do índice 1.
+    System.out.println(names.size()); // Utilizando o método size para pegar o tamanho da lista.
+
+    for (String name : names) {
+      System.out.println(name);
+    }
+
+    names.forEach(name -> System.out.println("Nome cadastrado: " + name));
+    // forma que foi intrduzida a partir do java 8 para percorrer uma lista.
+  }
+}
